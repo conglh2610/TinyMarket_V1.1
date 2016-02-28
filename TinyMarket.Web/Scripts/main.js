c4d85b89c0112ff8604da2bbb7bd04898b5ff8cb
conglh2610 <lhcongtk32@gmail.com>
2016-02-28 17:17:48 +0700

    update

---
+++ TinyMarket.Web/Scripts/main.js
@@ -0,0 +1,14 @@
+﻿require.config({
+    baseUrl: 'app',
+    urlArgs: 'v=1.0'
+});
+
+require(
+    [
+        'app'
+        ,'services/routeResolver'
+        , 'services/config'
+    ],
+    function () {
+        angular.bootstrap(document, ['myApp']);
+    });
